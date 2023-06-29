870798a6a0a833fb85a154894896290ea897c1f8
TeeCain <111044931+TeeCain@users.noreply.github.com>
2023-06-29 02:28:17 -0400

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,15 @@
+#Parts of Speech that user enters
+noun1 = input("Enter a noun: ")
+verb1 = input("Enter a verb: ")
+adjective1 = input("Enter an adjective: ")
+noun2 = input("Enter another noun: ")
+noun3 = input("Enter first noun: ")
+adjective2 = input("Enter another adjective: ")
+verb2 = input("Enter another verb: ")
+
+
+# Fill in the blanks in the story
+story = "The " + noun1 + " " + verb1 + " through the " + adjective1 + " forest with a " + noun2 + " in its hand. To the " + noun3 + "'s surprise, there was a " + adjective2 + " stranger "
++ verb2 + " nearby."
+# Display the story
+print(story)
